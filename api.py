023f0928710c6621f3e769c5c08f15cfea98cd71
Rushil Singh <rushilsingh@gmail.com>
2019-08-26 19:35:46 +0530

    Building parsing capabilities

--- api.py
+++ api.py
@@ -10,21 +10,36 @@ class BhavData(object):
 
     def __init__(self):
         self.url = API
+        self.fname = API.split("/")[-1]
         self.response = None
         self.content = None
     def download(self):
 
         while(self.response is None or self.response.status_code != 200):
             self.response = requests.get(self.url, stream=True)
-            content = self.response.content if self.response.status_code == 200 else None
+            content = self.response.text if self.response.status_code == 200 else None
             if content is not None:
                 if self.content is None or content != self.content:
                     self.content = content
+        self.parse()
 
+    def parse(self):
+        if data.content is None:
+            return
+        content = data.content.split("\n")
+        header = content[0].split(",")
+        content = content[1:]
+        parsed = []
+        for line in content:
+            values = line.split(",")
+            record = dict(zip(header, values))
+            parsed.append(record)
 
+        self.content = parsed
         
 
 
 if __name__ == '__main__':
     data = BhavData()
     data.download()
+    print(data.content) 
