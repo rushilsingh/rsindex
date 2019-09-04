d222838761897ebbd60a80ef69ff49796e22db8e
Rushil Singh <rushilsingh@gmail.com>
2019-09-04 21:23:13 +0530

    Added python file with stub for trading simulations

---
+++ simulation.py
@@ -0,0 +1,20 @@
+#  Simulate some trade based on some initial conditions and decisions based on relative strength based strategy
+
+"""
+Prospective constants:
+STARTING_CAPITAL = 1000
+INVESTED_CAPITAL = 0
+"""
+"""
+ROUGHLY:
+PURCHASE_AMOUNT = PRICE * UNITS
+INVESTED_CAPITAL += PURCHASE_AMOUNT
+STARTING_CAPITAL -= PURCHASE_AMOUNT
+TRADE is some buy/sell operation
+... and so on. something like this.
+"""
+import pickle
+
+with open("rsi_data.obj", "rb") as f:
+    obj = pickle.load(f)
+print(obj)
